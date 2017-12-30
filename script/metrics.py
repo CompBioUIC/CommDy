8b2c86482ae6e706f02d275ec9f3083bcacd844e
Chayant T15h <chayant@gmail.com>
2017-12-30 12:03:26 -0600

    add inquisitiveness

--- script/metrics.py
+++ script/metrics.py
@@ -18,10 +18,63 @@ def absenteeism(df, colmap=column_map()):
     for individual, group in df.groupby(['individual']):
         num_absent = 0
         for index, row in group.iterrows():
-            if row['group_color'] != row['individual_color'] and row['individual_color'] in time_group_colors[row['time']]:
+            my_group_color = row['group_color']
+            my_color = row['individual_color']
+            group_colors = time_group_colors[row['time']]
+            if my_group_color != my_color and my_color in group_colors:
+                # If I am not in the group of my color, I am absent from the
+                # group of my color.
                 num_absent += 1
         values.append(float(num_absent) / len(group))
     return values
+
+def inquisitiveness(df):
+    values = []
+    for individual, group in df.groupby(['individual']):
+        num_visit = 0
+        for index, row in group.iterrows():
+            my_group_color = row['group_color']
+            my_color = row['individual_color']
+            if my_group_color != my_color and pd.notna(my_group_color):
+                print("ind", individual, "visit")
+                print(row)
+                print()
+                num_visit += 1
+        values.append(float(num_visit) / len(group.dropna()))
+    return values
+
+def community_stay(df):
+    #comm.stay = mean(compute.stay(na.omit(x)))
+    pass
+
+def peer_synchrony(df):
+    #my.peers = merge(x, ig.color, by=c('time', 'i.color'), all.x=T, suffixes=c("", ".y"))
+    #peer = mean(ddply(my.peers, .(time), function(y) sum(y$ind!=y$ind.y))[,2])
+    #peer.synchrony = mean(ddply(my.peers, .(time), function(y) {
+    #    z = with(y[y$ind!=y$ind.y,], next.i.color==next.i.color.y)
+    #    ifelse (length(z)==0, 0, z)
+    #})[,2])
+    pass
+
+def group_size(df):
+    ##group.size = mean(merge(x, ddply(gtm, "group", nrow))$V1)
+    pass
+
+def individual_apparency(df):
+    #ind.apparancy = nrow(na.omit(x))/nrow(x)
+    pass
+
+def cyclickty(df):
+    #cyclicity = sum(table(x$i.color[which(c(T, diff(x$i.color)!=0))])-1)
+    pass
+
+def community_size(df):
+    ##comm.size = mean(ddply(merge(x, ig.color, by=c("time", "i.color")), "time", nrow)$V1)
+    pass
+
+def individual_number_of_communities(df):
+    #ind.num.comm = length(unique(x$i.color))
+    pass
 
 def compute_stats(df,
         t_col='time', g_col='group', i_col='individual',
--- script/metrics_test.py
+++ script/metrics_test.py
@@ -6,9 +6,9 @@ from metrics import *
 
 columns = 'time group individual group_color individual_color'.split(' ')
 
-class TestMetric(unittest.TestCase):
+class TestAbsenteeism(unittest.TestCase):
 
-    def test_absenteeism_none(self):
+    def test_none(self):
         df = pd.DataFrame(
             data={
                 'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
@@ -22,7 +22,7 @@ class TestMetric(unittest.TestCase):
         got = absenteeism(df)
         self.assertEqual(got, [0, 0], "got -> want")
 
-    def test_absenteeism_always(self):
+    def test_always(self):
         df = pd.DataFrame(
             data={
                 'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
@@ -36,7 +36,7 @@ class TestMetric(unittest.TestCase):
         got = absenteeism(df)
         self.assertEqual(got, [0, 1], "got -> want")
 
-    def test_absenteeism_sometimes(self):
+    def test_sometimes(self):
         df = pd.DataFrame(
             data={
                 'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
@@ -49,6 +49,50 @@ class TestMetric(unittest.TestCase):
         )
         got = absenteeism(df)
         self.assertEqual(got, [0.5, 0.0], "got -> want")
+
+class TestInquisitiveness(unittest.TestCase):
+
+    def test_none(self):
+        df = pd.DataFrame(
+            data={
+                'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
+                'group'            : ['g1', 'g2', 'g1', 'g2', 'g1', 'g2', 'g1', 'g2'],
+                'individual'       : ['i1', 'i2', 'i1', 'i2', 'i1', 'i2', 'i1', 'i2'],
+                'group_color'      : [   1,    2,    1,    2,    1,    2,    1,    2],
+                'individual_color' : [   1,    2,    1,    2,    1,    2,    1,    2],
+            },
+            columns = columns,
+        )
+        got = inquisitiveness(df)
+        self.assertEqual(got, [0, 0], "got -> want")
+
+    def test_always(self):
+        df = pd.DataFrame(
+            data={
+                'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
+                'group'            : ['g1', 'g2', 'g1', 'g2', 'g1', 'g2', 'g1', 'g2'],
+                'individual'       : ['i1', 'i2', 'i1', 'i2', 'i1', 'i2', 'i1', 'i2'],
+                'group_color'      : [   1,    2,    1,    2,    1,    2,    1,    2],
+                'individual_color' : [   1,    1,    1,    1,    1,    1,    1,    1],
+            },
+            columns = columns,
+        )
+        got = inquisitiveness(df)
+        self.assertEqual(got, [0, 1], "got -> want")
+
+    def test_absent_is_not_visit(self):
+        df = pd.DataFrame(
+            data={
+                'time'             : ['t1', 't1', 't2', 't2', 't3', 't3', 't4', 't4'],
+                'group'            : ['g1', 'g1', None, 'g1', 'g1', 'g2', 'g1', 'g1'],
+                'individual'       : ['i1', 'i2', 'i1', 'i2', 'i1', 'i2', 'i1', 'i2'],
+                'group_color'      : [   1,    1, None,    1,    1,    2,    1,    1],
+                'individual_color' : [   1,    1,    1,    1,    2,    2,    1,    1],
+            },
+            columns = columns,
+        )
+        got = inquisitiveness(df)
+        self.assertEqual(got, [1./3, 0.0], "got -> want")
 
 if __name__ == '__main__':
     unittest.main()
