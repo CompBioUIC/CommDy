d13f62210ea35ada17d7cb435bd7d882085a6486
Chayant T15h <chayant@gmail.com>
2018-08-09 18:21:44 -0500

    rename columns to icolor and gcolor

--- script/metrics.py
+++ script/metrics.py
@@ -6,8 +6,8 @@ def column_map():
         'time': 'time',
         'group': 'group',
         'individual': 'individual',
-        'group_color': 'group_color',
-        'individual_color': 'individual_color',
+        'gcolor': 'gcolor',
+        'icolor': 'icolor',
     }
 
 # Return the pairs of adjacent items.
@@ -24,17 +24,17 @@ def avg(items):
 
 # absenteeism = % times an individual is absent from his/her community's groups.
 def absenteeism(df, colmap=column_map()):
-    time_group_colors = {
-            t: set(group['group_color'].values)
-            for t, group in df[['time', 'group_color']].drop_duplicates().dropna().groupby('time')}
+    time_gcolors = {
+            t: set(group['gcolor'].values)
+            for t, group in df[['time', 'gcolor']].drop_duplicates().dropna().groupby('time')}
     values = []
     for _, group in df.groupby(['individual']):
         num_absent = 0
         for index, row in group.iterrows():
-            my_group_color = row['group_color']
-            my_color = row['individual_color']
-            group_colors = time_group_colors[row['time']]
-            if my_group_color != my_color and my_color in group_colors:
+            my_gcolor = row['gcolor']
+            my_color = row['icolor']
+            gcolors = time_gcolors[row['time']]
+            if my_gcolor != my_color and my_color in gcolors:
                 # If I am not in the group of my color, I am absent from the
                 # group of my color.
                 num_absent += 1
@@ -48,9 +48,9 @@ def inquisitiveness(df):
     for _, group in df.groupby(['individual']):
         num_visit = 0
         for index, row in group.iterrows():
-            my_group_color = row['group_color']
-            my_color = row['individual_color']
-            if my_group_color != my_color and pd.notna(my_group_color):
+            my_gcolor = row['gcolor']
+            my_color = row['icolor']
+            if my_gcolor != my_color and pd.notna(my_gcolor):
                 num_visit += 1
         values.append(float(num_visit) / len(group.dropna()))
     return values
@@ -61,7 +61,7 @@ def community_stay(df):
     values = []
     for _, group in df.groupby(['individual']):
         xs = []
-        my_colors = group['individual_color'].tolist()
+        my_colors = group['icolor'].tolist()
         if len(my_colors) == 0:
             values.append(0.0)
             continue
@@ -80,7 +80,7 @@ def community_stay(df):
 def compute_subgroups(df):
     community_subgroups = {}
     for _, row in df.dropna().iterrows():
-        t, g, i, c = [row[x] for x in ['time', 'group', 'individual', 'individual_color']]
+        t, g, i, c = [row[x] for x in ['time', 'group', 'individual', 'icolor']]
         if (t, g, c) in community_subgroups:
             community_subgroups[t, g, c].add(i)
         else:
@@ -96,7 +96,7 @@ def avg_num_peers(df, community_subgroups):
         for _, row in group.dropna().iterrows():
             g = row['group']
             t = row['time']
-            ic = row['individual_color']
+            ic = row['icolor']
             xs.append(len(community_subgroups[t, g, ic]) - 1)
         values.append(avg(xs))
     return values
@@ -108,8 +108,8 @@ def peer_synchrony(df, community_subgroups):
     for _, group in df.groupby(['individual']):
         xs = []
         for (_, prev_row), (_, curr_row) in adjacent_pairs(group.iterrows()):
-            t1, g1, c1 = prev_row['time'], prev_row['group'], prev_row['individual_color']
-            t2, g2, c2 = curr_row['time'], curr_row['group'], curr_row['individual_color']
+            t1, g1, c1 = prev_row['time'], prev_row['group'], prev_row['icolor']
+            t2, g2, c2 = curr_row['time'], curr_row['group'], curr_row['icolor']
             if pd.isnull(g1) or pd.isnull(g2):
                 xs.append(0.0)
                 continue
@@ -134,7 +134,7 @@ def group_homogeneity(df):
     d = {
         'time' : df['time'],
         'group' : df['group'],
-        'homogeneity': (df['group_color'] == df['individual_color']).astype(float),
+        'homogeneity': (df['gcolor'] == df['icolor']).astype(float),
     }
     h = pd.DataFrame(d).dropna().groupby(['time', 'group'])['homogeneity'].mean().to_dict()
     values = []
@@ -147,30 +147,30 @@ def individual_apparency(df):
     def diff(t2, t1):
         return time_index[t2] - time_index[t1] + 1
     community_apparency = {ic: diff(max(group['time']), min(group['time']))
-        for ic, group in df.groupby('individual_color')}
+        for ic, group in df.groupby('icolor')}
     values = []
     for _, group in df.groupby('individual'):
-        values.append(avg([community_apparency[ic] for ic in set(group['individual_color'].values)]))
+        values.append(avg([community_apparency[ic] for ic in set(group['icolor'].values)]))
     return values
 
 def cyclicity(df):
     values = []
     for i, group in df.groupby('individual'):
-        colors = group['individual_color']
+        colors = group['icolor']
         consecutive_uniques = colors[colors.shift() != colors]
-        value = consecutive_uniques.count() - group['individual_color'].nunique()
+        value = consecutive_uniques.count() - group['icolor'].nunique()
         values.append(value)
     return values
 
 def community_size(df):
-    size = df.groupby(['time', 'individual_color'])['individual'].count().to_dict()
+    size = df.groupby(['time', 'icolor'])['individual'].count().to_dict()
     values = []
-    value = df.groupby(['time', 'individual_color'])['individual_color'].count().mean()
+    value = df.groupby(['time', 'icolor'])['icolor'].count().mean()
     for _, group in df.groupby('individual'):
         xs = []
         for _, row in group.iterrows():
             t = row['time']
-            c = row['individual_color']
+            c = row['icolor']
             xs.append(size[t, c])
         values.append(avg(xs))
     return values
